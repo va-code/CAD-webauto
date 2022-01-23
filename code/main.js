99dc2c3f0bb30f0ee15cf813a6796531ff09a3c6
Vaughn <44816847+va-code@users.noreply.github.com>
2022-01-23 11:30:30 -0600

    working with /gitclone-glb/
    Everything hard coded, next step is making everything dynamic to file system and contained files.

--- code/main.js
+++ code/main.js
@@ -2,16 +2,52 @@ import './style.css';
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 document.querySelector('#app').innerHTML = `
-  <div>
-    <h1>Finally!</h1>
-    <a href="http://www.nuex.tech" target="_blank">old site</a>
-  </div>
-
-    <canvas id='my_canvas'></canvas>
+  <h1>Getting Closer!</h1> 
+  <a href="/" onclick="route()">Home</a>
+  <a href="/Project1" onclick="route()">Project1</a>
+  <a href="/Project2" onclick="route()">Project2</a>
+  <a href="/Project3" onclick="route()">Project3</a>
+  <canvas id='my_canvas'></canvas>
 `
 
 
-// threejs stuff after this point
+// routing start stuff after this point, needs to be in front of threejs because of animation
+
+
+
+const route = (event) => {
+	event = event || window.event;
+	event.preventDefault();
+	window.history.pushState({}, "", event.target.href);
+	handleLocation();
+};
+
+const routes = {
+	404: "/gitclone-glb/Home/404.html",
+	"/": "/gitclone-glb/Home/ProjTxt.txt",
+	"/Project1": "/gitclone-glb/Project1/ProjTxt.txt",
+	"/Project2": "/gitclone-glb/Project2/ProjTxt.txt",
+	"/Project3": "/gitclone-glb/Project3/ProjTxt.txt",
+
+
+};
+
+const handleLocation = async () => {
+	const path = window.location.pathname;
+	const route = routes[path] || routes[404];
+	const html = await fetch(route).then((data) => data.text());
+	document.querySelector('#swappable').innerHTML = html;
+
+};
+
+window.onpopstate = handleLocation;
+window.route = route;
+handleLocation();
+
+
+// routing end here
+
+// threejs start stuff after this point
 
 var renderer = new THREE.WebGLRenderer( { canvas: my_canvas } );
 renderer.setPixelRatio( window.devicePixelRatio );
@@ -64,5 +100,5 @@ function animate() {
 }
 animate();
 
-
+// threejs end here
 
