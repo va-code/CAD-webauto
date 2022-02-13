54c1c83f9f8fe05ef2519971c3bec04c7129bdbb
Vaughn <44816847+va-code@users.noreply.github.com>
2022-02-12 22:02:40 -0600

    Removed class structure in favor of appending meshes to list
    The class structure had some overhead because of naming each class instance so I switched it over to a function that returns the mesh object to a list. I also got all the pages loading different mesh files. As a temporary measure I made the mesh locations random(I will change it to pull from a json file later so each page has all meshes placed in a reasonable way)

--- code/main.js
+++ code/main.js
@@ -7,13 +7,13 @@ const route = (event) => {
 	event.preventDefault();
 	window.history.pushState({}, "", event.target.href);
 	handleLocation();
-};
+	loadfiles();
+};  
 const routes = await fetch("/gitclone-glb/Routes.json").then((data) => data.json());
 const handleLocation = async () => {
 	const path = window.location.pathname;
 	const route = routes["Routes"][path] || routes["Routes"][404];
 	const html = await fetch(route).then((data) => data.text());
-	//if (route =="gitclone-glb/Project1/") {for glb in glbs import file.glb as filename};
 	document.querySelector('#swappable').innerHTML = html;
 };
 window.onpopstate = handleLocation;
@@ -27,54 +27,47 @@ document.querySelector('#app').innerHTML = hrefs["hrefs"]
 var renderer = new THREE.WebGLRenderer( { canvas: my_canvas } );
 renderer.setPixelRatio( window.devicePixelRatio );
 const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
-camera.position.set(0 , 0, 200);
-camera.lookAt( 0, 0, 0 );
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0x202124);
-var light = new THREE.PointLight(0xffffff,0.5);
-light.position.y = 100;
-light.position.z = 40;
-scene.add(light);
-
-class glbfile {
-	constructor(filepath, scene, Xpos, Ypos, scaling){
-		this.Xpos = Xpos;
-		this.Ypos = Ypos;
-		this.scaling = scaling;
-		this.object = new THREE.Group();
-		(new GLTFLoader()).load(filepath, gltf => {
-				this.object.add(gltf.scene);
-			});
-		this.object.scale.set(scaling,scaling,scaling);
-		this.object.position.x = Xpos;
-		this.object.position.y = Ypos;
-	};
-	
-	draw(rotationX, rotationY) {
-		this.object.rotation.x = rotationX;
-		this.object.rotation.y = rotationY;
-	};
+function scenesetup(){
+	camera.position.set(0 , 0, 200);
+	camera.lookAt( 0, 0, 0 );
+	scene.background = new THREE.Color(0x202124);
+	var light = new THREE.PointLight(0xffffff,0.5);
+	light.position.y = 100;
+	light.position.z = 40;
+	scene.add(light);
 };
+scenesetup();
+function meshgen(filepath, scene, Xpos, Ypos, scaling){
+	var object = new THREE.Group();
+	(new GLTFLoader()).load(filepath, gltf => {
+		object.add(gltf.scene);
+		});
+	object.position.x = Xpos;
+	object.position.y = Ypos;
+	object.scale.set(scaling,scaling,scaling);
+	scene.add(object);
+	return object;
+};
+var listed = [];
+const loadfiles = async () =>{
+	const path = window.location.pathname;
+	const glbstr = routes["glb"][path] || routes["glb"][404];
+	const glb = glbstr.split(" ")
+	scene.clear();
+	scenesetup();
+	glb.map(function(meshpath){
+		listed.push(meshgen(meshpath, scene, Math.random()*100 -50 , Math.random()*100 -50, 3));
+		});
+};
+loadfiles(route);
 var test = 0;
-let letter_A = new glbfile('/gitclone-glb/Project1/A.glb', scene, -50, 0, 3);
-let letter_B = new glbfile('/gitclone-glb/Project1/B.glb', scene, -25, 0, 3);
-let letter_C = new glbfile('/gitclone-glb/Project1/C.glb', scene, 0, 0, 3);
-//testing zone, careful where you step...
-
-scene.add(letter_A.object);
-scene.add(letter_B.object);
-scene.add(letter_C.object);
-//testing zone, careful where you step...
 function animate() {
 	requestAnimationFrame( animate );
 	test += 0.01;
-	
-	letter_A.draw(test, test);
-	letter_B.draw(test, test);
-	letter_C.draw(test, test);
+	listed.map(mesh => (mesh.rotation.x = test));
+	listed.map(mesh => (mesh.rotation.y = -test));
 	renderer.render( scene, camera );
 }
 animate();
-
 // threejs end here
-
